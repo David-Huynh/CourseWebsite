81e8df6fede3719825d5292a3e4c030561d73d57
david-huynh <huynh.david.work@gmail.com>
2021-03-04 16:20:48 -0500

    Finished webserver for Question 1

---
+++ Question1.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/<username>')
+def generateResponse(username):
+    digitFree = ''
+    for char in username:
+        if not char.isdigit():
+            digitFree += char
+    return 'Welcome, %s, to my CSCB20 website!' % digitFree
+
+if __name__ == "__main__":
+    app.run(debug=True)
