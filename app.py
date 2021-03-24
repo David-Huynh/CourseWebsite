e373f5eee12ce87d06ea9aebf986357ad53bb4c4
david-huynh <huynh.david.work@gmail.com>
2021-03-23 23:09:51 -0400

    Updated files to match assignment requirements

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/<username>')
-def generateResponse(username):
-    digitFree = ''
-    for char in username:
-        if not char.isdigit():
-            digitFree += char
+@app.route('/')
+def home():
     return 'Welcome, %s, to my CSCB20 website!' % digitFree
 
 if __name__ == "__main__":
