294b9144a6bc7e367896e4bb423d7c70b249b48a
Collin Chan <chancollin5101@gmail.com>
2021-04-03 21:59:12 -0400

    added edit account modal in profile.html

--- src/static/javascript/profile.js
+++ src/static/javascript/profile.js
@@ -1,20 +1,33 @@
 window.onload = function() {
+    const acc_modal = document.getElementById("account_modal");
+    const acc_btn = document.getElementById("editA");
+    const span1 = document.getElementsByClassName("close")[0];
 
-    const modal = document.getElementById("myModal");
-    const btn = document.getElementById("editB");
-    const span = document.getElementsByClassName("close")[0];
+    const prof_modal = document.getElementById("profile_modal");
+    const prof_btn = document.getElementById("editB");
+    const span2 = document.getElementsByClassName("close")[1];
 
-    btn.onclick = function(event) {
-        modal.style.display = "block";
+    acc_btn.onclick = function(event) {
+        acc_modal.style.display = "block";
     }
 
-    span.onclick = function(event) {
-        modal.style.display = "none";
+    span1.onclick = function(event) {
+        acc_modal.style.display = "none";
+    }
+   
+    prof_btn.onclick = function(event) {
+        prof_modal.style.display = "block";
+    }
+
+    span2.onclick = function(event) {
+        prof_modal.style.display = "none";
     }
 
     window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+        if (event.target == prof_modal) {
+            prof_modal.style.display = "none";
+        } else if (event.target == acc_modal) {
+            acc_modal.style.display = "none";
         }
     }
 }
