9c17cb3171d94e7ffd7daf61f695e3605fcd0c3d
Collin Chan <chancollin5101@gmail.com>
2021-04-04 18:10:51 -0400

    added navbtn for nav bar for more responsive design

---
+++ src/static/javascript/template.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    const navBtn = document.getElementById("barBtn")
+    const links = document.querySelector(".desktopList")
+
+    navBtn.onclick = function(event) {
+        console.log(event)
+        links.classList.toggle("navActive")
+
+    }
+}
